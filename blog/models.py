407c061b756e64511d856ee2e7af4dc8878580f9
Matthew Scott <mjs@mjs-svc.com>
2010-08-26 14:28:30 -0600

    Blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Entry(models.Model):
+    owner = models.ForeignKey(User)
+    ctime = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length = 250)
+    body = models.TextField()
